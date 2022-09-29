7fd2105475044892f511bd8a604fe2a527634f3f
Gerry Bennett <gerry.bennett@ninjaone.com>
2022-09-29 09:21:00 -0500

    fix: cleanup of scripts adding shebang

--- scripts/build.sh
+++ scripts/build.sh
@@ -18,12 +18,42 @@ docker stop ${BUILD_IMAGE} 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
 docker rm ${BUILD_IMAGE} 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
 docker rmi ${IMAGE} 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
 
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +pgbouncer-binary" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +pgbouncer-binary 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
+
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +pgbouncer-package" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +pgbouncer-package 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
+
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +generate-pgp-key" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +generate-pgp-key 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
+
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +create-repo" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +create-repo 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
+
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +repo-server" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +repo-server 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
+
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +install-pgbouncer" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +install-pgbouncer 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
+
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "earthly -P +pack" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 earthly -P +pack 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
 
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
+echo "docker load -i ${DIST_DIR}/${TAR_BUILD}" >> ${DIST_DIR}/logs/earthly_build.log
+echo "********************************************************************************" >> ${DIST_DIR}/logs/earthly_build.log
 docker load -i ${DIST_DIR}/${TAR_BUILD} 2>&1 | tee -a ${DIST_DIR}/logs/earthly_build.log
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,1 +1,8 @@
-drit --entrtypoint bash -u 0 --rm --name test test/pgbouncer
+#!/usr/bin/env sh
+
+set -eu
+set -o pipefail
+
+############################################################
+alias drit='docker run -it'
+drit --entrypoint bash -u 0 --rm --name test test/pgbouncer
